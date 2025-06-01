244af6eaffd756729489dd42e44ea891401d591a
tphuc <nguoigiaumat100@gmail.com>
2025-06-01 18:05:06 +0700

    feat: add basic main function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thanhphuchuynh/dear-future
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a placeholder for the main function.
+	// You can add your code here to implement the desired functionality.
+	fmt.Println("Hello, World!")
+}
